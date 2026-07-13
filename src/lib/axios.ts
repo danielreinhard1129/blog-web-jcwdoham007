d11968471c080384bddd8ea32b08b4048d864b31
danielreinhard1129 <danielreinhard1129@gmail.com>
2026-07-13 11:04:21 +0700

    feat: add refresh token

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,13 +3,33 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL_API,
+  withCredentials: true,
 });
 
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = useAuth.getState().user?.accessToken;
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
+export const refreshInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL_API,
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
   },
-  (error) => Promise.reject(error),
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response.status === 401 &&
+      error.response.data.message === "Token expired" &&
+      !originalRequest.retry
+    ) {
+      try {
+        await refreshInstance.post("/auth/refresh");
+        return axiosInstance(originalRequest);
+      } catch (error) {
+        useAuth.getState().logout();
+        return Promise.reject(error);
+      }
+    }
+  },
 );
