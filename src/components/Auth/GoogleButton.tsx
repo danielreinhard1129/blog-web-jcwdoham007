87b000c5602203ee57a0c8f15c19d130bd38e067
danielreinhard1129 <danielreinhard1129@gmail.com>
2026-07-14 10:31:20 +0700

    feat: add login by google

--- src/components/Auth/GoogleButton.tsx
+++ src/components/Auth/GoogleButton.tsx
@@ -1,11 +1,42 @@
 import GoogleIcon from "@/components/icons/GoogleIcon";
+import { axiosInstance } from "@/lib/axios";
+import { useAuth } from "@/stores/useAuth";
+import { useGoogleLogin } from "@react-oauth/google";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router";
 
 function GoogleButton() {
+  const { login } = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleLogin = useGoogleLogin({
+    onSuccess: async ({ access_token }) => {
+      try {
+        const response = await axiosInstance.post("/auth/google", {
+          accessToken: access_token,
+        });
+
+        login({
+          id: response.data.id,
+          name: response.data.name,
+          email: response.data.email,
+          profilePic: response.data.profilePic,
+          role: response.data.role,
+        });
+
+        navigate("/");
+      } catch {
+        toast.error("Login failed!");
+      }
+    },
+  });
+
   return (
     <button
-      disabled
       type="button"
       className="w-full h-11 flex items-center justify-center gap-2.5 rounded-lg border border-border bg-background text-sm font-medium hover:bg-surface transition-colors"
+      onClick={() => handleLogin()}
     >
       <GoogleIcon />
       Continue with Google
